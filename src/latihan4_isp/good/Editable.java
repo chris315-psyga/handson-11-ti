65a7b57005d30dc98fde1f4016519ec0599afff2
chris maruli <chris.maruli7@gmail.com>
2025-11-21 10:33:33 +0700

    Progress awal handson-11

---
+++ src/latihan4_isp/good/Editable.java
@@ -0,0 +1,17 @@
+package latihan4_isp.good;
+
+// Interface untuk capability EDITING
+// HANYA document yang bisa diedit yang implement ini
+public interface Editable {
+    void edit(String content);
+    // TODO: Uncomment dan implementasikan
+    /*
+     * Edit document content
+     * Hanya Word dan Spreadsheet yang implement ini
+     *
+     * @param content - New content to write
+     */
+    /*
+    void edit(String content);
+    */
+}
---
+++ src/latihan4_isp/good/Printable.java
@@ -0,0 +1,15 @@
+package latihan4_isp.good;
+
+// Interface untuk capability PRINTING
+// PDF, Word, Spreadsheet bisa print. Image tidak.
+public interface Printable {
+    void print();
+    // TODO: Uncomment dan implementasikan
+    /*
+     * Print document
+     * Hanya document yang bisa dicetak yang implement ini
+     */
+    /*
+    void print();
+    */
+}
